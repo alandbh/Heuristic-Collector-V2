fefb7a7ca7197060d000238455d45d6312c56344
Alan Vasconcelos <alandbh@gmail.com>
2022-08-28 23:28:42 -0300

    Begining styling

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,16 +1,28 @@
+import Image from "next/image";
 import Link from "next/link";
 
 function Header({ router }) {
     return (
-        <>
-            <h1>PAGE HEADER</h1>
+        <header className="bg-blue-500 flex justify-between px-5">
+            <Link href={`/`}>
+                <a>
+                    <div className="py-1">
+                        <Image
+                            src={`/logo-white-35.svg`}
+                            width={100}
+                            height={35}
+                            alt={`Back to Project Gallery`}
+                        />
+                    </div>
+                </a>
+            </Link>
             <nav>
                 <Link href={`/project/${router.slug}`}>Evaluation</Link>
                 <Link href={`/project/${router.slug}?tab=${router.tab}`}>
                     Dashboard
                 </Link>
             </nav>
-        </>
+        </header>
     );
 }
 
