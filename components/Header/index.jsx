409173d5a15ea14c0906d335fb74bd5d76c7a138
Alan Vasconcelos <alandbh@gmail.com>
2022-08-30 23:31:24 -0300

    Header responsiveness

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useProjectContext } from "../../context/project";
+import JourneySelect from "../JourneySelect";
 import PlayerSelect from "../PlayerSelect";
 
 function Header({ routes }) {
@@ -12,15 +13,22 @@ function Header({ routes }) {
 
     const { project } = useProjectContext();
 
-    // console.log("PROJECT", project);
+    function handleNav(param, value) {
+        let _query = { ...router.query };
+        _query[param] = value;
 
-    const LINK_CLASSES = `border flex gap-2 align-middle items-center py-1 px-5 rounded-full transition-all text-sm `;
+        router.replace({
+            query: _query,
+        });
+    }
+
+    const LINK_CLASSES = `border flex gap-2 align-middle items-center py-1 px-4 md:px-5 rounded-full transition-all text-xs md:text-sm `;
     return (
         <header>
-            <div className="bg-primary flex justify-between px-5 items-center">
+            <div className="bg-primary flex justify-between px-5 items-center h-10">
                 <Link href={`/`}>
                     <a>
-                        <div className="py-1">
+                        <div className="py-1 hidden sm:block">
                             <Image
                                 src={`/logo-white-35.svg`}
                                 width={100}
@@ -28,65 +36,77 @@ function Header({ routes }) {
                                 alt={`Back to Project Gallery`}
                             />
                         </div>
+                        <div className="block sm:hidden bg-red-600 h-6 w-6"></div>
                     </a>
                 </Link>
                 <nav className="border-white/50 border rounded-full flex font-bold text-white">
-                    <Link href={`/project/${routes.slug}`}>
-                        <a>
-                            <span
-                                className={`${
-                                    isDash
-                                        ? "border-transparent opacity-60"
-                                        : "border-white text-white opacity-100"
-                                } ${LINK_CLASSES} `}
+                    {/* <Link href={`/project/${routes.slug}`}> */}
+                    {/* <a href={`/project/${routes.slug}`}> */}
+                    <button
+                        onClick={() => {
+                            handleNav("tab", "");
+                        }}
+                    >
+                        <span
+                            className={`${
+                                isDash
+                                    ? "border-transparent opacity-60"
+                                    : "border-white text-white opacity-100"
+                            } ${LINK_CLASSES} `}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="17"
+                                fill="none"
+                                viewBox="0 0 16 17"
                             >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="16"
-                                    height="17"
-                                    fill="none"
-                                    viewBox="0 0 16 17"
-                                >
-                                    <path
-                                        fill="#fff"
-                                        d="M13.92 12.521a2.676 2.676 0 00-5.312.26c-2.742-.085-3.715-.726-3.556-2.03.059-.487.353-.747.894-.874A3.29 3.29 0 016.97 9.82c.03.003.062.005.092.009.202.048.408.089.613.121.609.1 1.228.15 1.818.118 1.657-.09 2.785-.82 2.843-2.37.055-1.502-.977-2.566-2.641-3.278-.534-.229-1.1-.406-1.667-.54a9.722 9.722 0 00-.628-.128 2.676 2.676 0 00-5.32-.045 2.676 2.676 0 005.177 1.229c.168.03.334.064.5.103a8.848 8.848 0 011.471.475c1.263.54 1.952 1.252 1.92 2.141-.028.797-.6 1.165-1.718 1.225a7.319 7.319 0 01-1.559-.103 7.906 7.906 0 01-.403-.077c-.07-.014-.118-.027-.14-.032l-.055-.01a4.46 4.46 0 00-1.593.059c-.985.23-1.677.842-1.805 1.888-.28 2.254 1.403 3.299 4.928 3.374a2.677 2.677 0 005.116-1.458zM4.733 5.518a1.488 1.488 0 110-2.975 1.488 1.488 0 010 2.975zm6.544 8.925a1.489 1.489 0 110-2.978 1.489 1.489 0 010 2.978z"
-                                    ></path>
-                                </svg>{" "}
-                                Evaluation
-                            </span>
-                        </a>
-                    </Link>
-                    <Link href={`/project/${routes.slug}?tab=${routes.tab}`}>
-                        <a>
-                            <span
-                                className={`${
-                                    isDash
-                                        ? "border-white opacity-100"
-                                        : "border-transparent opacity-60"
-                                } ${LINK_CLASSES}`}
+                                <path
+                                    fill="#fff"
+                                    d="M13.92 12.521a2.676 2.676 0 00-5.312.26c-2.742-.085-3.715-.726-3.556-2.03.059-.487.353-.747.894-.874A3.29 3.29 0 016.97 9.82c.03.003.062.005.092.009.202.048.408.089.613.121.609.1 1.228.15 1.818.118 1.657-.09 2.785-.82 2.843-2.37.055-1.502-.977-2.566-2.641-3.278-.534-.229-1.1-.406-1.667-.54a9.722 9.722 0 00-.628-.128 2.676 2.676 0 00-5.32-.045 2.676 2.676 0 005.177 1.229c.168.03.334.064.5.103a8.848 8.848 0 011.471.475c1.263.54 1.952 1.252 1.92 2.141-.028.797-.6 1.165-1.718 1.225a7.319 7.319 0 01-1.559-.103 7.906 7.906 0 01-.403-.077c-.07-.014-.118-.027-.14-.032l-.055-.01a4.46 4.46 0 00-1.593.059c-.985.23-1.677.842-1.805 1.888-.28 2.254 1.403 3.299 4.928 3.374a2.677 2.677 0 005.116-1.458zM4.733 5.518a1.488 1.488 0 110-2.975 1.488 1.488 0 010 2.975zm6.544 8.925a1.489 1.489 0 110-2.978 1.489 1.489 0 010 2.978z"
+                                ></path>
+                            </svg>{" "}
+                            Evaluation
+                        </span>
+                    </button>
+                    {/* </a> */}
+                    {/* </Link> */}
+                    {/* <Link href={`/project/${routes.slug}?tab=${routes.tab}`}> */}
+                    <button
+                        onClick={() => {
+                            handleNav("tab", routes.tab);
+                        }}
+                    >
+                        <span
+                            className={`${
+                                isDash
+                                    ? "border-white opacity-100"
+                                    : "border-transparent opacity-60"
+                            } ${LINK_CLASSES}`}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="17"
+                                fill="none"
+                                viewBox="0 0 16 17"
                             >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="16"
-                                    height="17"
-                                    fill="none"
-                                    viewBox="0 0 16 17"
-                                >
-                                    <path
-                                        fill="#fff"
-                                        d="M14.714 13.5H2.428V2.359a.143.143 0 00-.143-.143h-1a.143.143 0 00-.142.143v12.285c0 .079.064.143.142.143h13.429a.143.143 0 00.143-.143v-1a.143.143 0 00-.143-.142zM4 12.073h1a.143.143 0 00.143-.143V9.358A.143.143 0 005 9.215H4a.143.143 0 00-.143.143v2.571c0 .079.064.143.143.143zm2.714 0h1a.143.143 0 00.143-.143V6.215a.143.143 0 00-.143-.143h-1a.143.143 0 00-.143.143v5.714c0 .079.064.143.143.143zm2.714 0h1a.143.143 0 00.143-.143V7.608a.143.143 0 00-.143-.143h-1a.143.143 0 00-.143.143v4.321c0 .079.065.143.143.143zm2.715 0h1a.143.143 0 00.142-.143V4.786a.143.143 0 00-.142-.143h-1a.143.143 0 00-.143.143v7.143c0 .079.064.143.143.143z"
-                                    ></path>
-                                </svg>
-                                Dashboard
-                            </span>
-                        </a>
-                    </Link>
+                                <path
+                                    fill="#fff"
+                                    d="M14.714 13.5H2.428V2.359a.143.143 0 00-.143-.143h-1a.143.143 0 00-.142.143v12.285c0 .079.064.143.142.143h13.429a.143.143 0 00.143-.143v-1a.143.143 0 00-.143-.142zM4 12.073h1a.143.143 0 00.143-.143V9.358A.143.143 0 005 9.215H4a.143.143 0 00-.143.143v2.571c0 .079.064.143.143.143zm2.714 0h1a.143.143 0 00.143-.143V6.215a.143.143 0 00-.143-.143h-1a.143.143 0 00-.143.143v5.714c0 .079.064.143.143.143zm2.714 0h1a.143.143 0 00.143-.143V7.608a.143.143 0 00-.143-.143h-1a.143.143 0 00-.143.143v4.321c0 .079.065.143.143.143zm2.715 0h1a.143.143 0 00.142-.143V4.786a.143.143 0 00-.142-.143h-1a.143.143 0 00-.143.143v7.143c0 .079.064.143.143.143z"
+                                ></path>
+                            </svg>
+                            Dashboard
+                        </span>
+                    </button>
+                    {/* </Link> */}
                 </nav>
             </div>
             <div className="bg-white shadow-md px-5 py-3">
-                <div className="flex items-center gap-5">
-                    <b>{project.name}</b>
+                <div className="flex items-center gap-5 justify-between">
+                    <b className="hidden sm:inline">{project.name}</b>
                     <svg
+                        className="hidden sm:inline"
                         width="21"
                         height="54"
                         viewBox="0 0 21 54"
@@ -103,6 +123,7 @@ function Header({ routes }) {
                     </svg>
 
                     <PlayerSelect />
+                    <JourneySelect />
                 </div>
             </div>
         </header>
