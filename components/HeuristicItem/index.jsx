10802ef807b799ae42d7d875468178101b0ef56f
Alan Vasconcelos <alandbh@gmail.com>
2022-09-01 13:17:20 -0300

    Adding Note component OK

--- components/HeuristicItem/index.jsx
+++ components/HeuristicItem/index.jsx
@@ -27,6 +27,7 @@ const MUTATION_PUBLIC = gql`
         publishScore(where: { id: $scoreId }, to: PUBLISHED) {
             id
             scoreValue
+            note
         }
     }
 `;
@@ -40,7 +41,7 @@ const processChange = debounce(
         console.log("Saving data func");
         doMutate(variables);
     },
-    1000,
+    2000,
     false
 );
 
@@ -64,9 +65,21 @@ async function doPublic(variables) {
     return data;
 }
 
+/**
+ *
+ *
+ * Begining of the component
+ * ----------------------------------
+ *
+ * @param {*props} param0
+ * @returns Heuristic Item with Range and Note
+ */
+
 function HeuristicItem({ heuristic }) {
     const [score, setScore] = useState(0);
+    const [text, setText] = useState(currentScore?.note);
     const { scores } = useScoresContext();
+    const [boxOpen, setBoxOpen] = useState(false);
 
     const currentScore = scores.find(
         (score) => score.heuristic.heuristicNumber === heuristic.heuristicNumber
@@ -75,6 +88,7 @@ function HeuristicItem({ heuristic }) {
     useEffect(() => {
         if (currentScore !== undefined) {
             setScore(currentScore.scoreValue);
+            setText(currentScore.note);
         } else {
             setScore(0);
         }
@@ -92,20 +106,30 @@ function HeuristicItem({ heuristic }) {
         processChange({
             scoreId: currentScore.id,
             scoreValue: Number(ev.target.value),
-            scoreNote: "nota interna2",
+            scoreNote: currentScore.note,
+        });
+    }
+
+    function handleChangeText(newText) {
+        setText(newText);
+        console.log(text);
+
+        processChange({
+            scoreId: currentScore.id,
+            scoreValue: currentScore.scoreValue,
+            scoreNote: newText,
         });
     }
 
     return (
-        <li className="flex">
+        <li className="flex mb-10 gap-5 max-w-lg">
             <div>
-                <b>{heuristic.heuristicNumber}</b>
+                <b className="text-xl">{heuristic.heuristicNumber}</b>
             </div>
             <div>
-                <h2>{heuristic.name}</h2>
-                <p>{heuristic.description}</p>
+                <h2 className="text-lg mb-2 font-bold">{heuristic.name}</h2>
+                <p className="text-sm">{heuristic.description}</p>
 
-                <div>SLIDER</div>
                 <Range
                     type={"range"}
                     min={0}
@@ -113,10 +137,48 @@ function HeuristicItem({ heuristic }) {
                     value={score}
                     onChange={(ev) => handleChangeRange(ev)}
                 />
-                <button>Add Note</button>
+                <button
+                    className="font-bold py-1 pr-3 text-sm text-primary flex gap-2"
+                    onClick={() => setBoxOpen(!boxOpen)}
+                >
+                    <svg
+                        width="20"
+                        height="23"
+                        viewBox="0 0 20 23"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M2 0.221802H18V12.2218H16V2.2218H2V18.2218H10V20.2218H0V0.221802H2ZM4 4.2218H14V6.2218H4V4.2218ZM14 8.2218H4V10.2218H14V8.2218ZM4 12.2218H11V14.2218H4V12.2218ZM17 17.2218H20V19.2218H17V22.2218H15V19.2218H12V17.2218H15V14.2218H17V17.2218Z"
+                            fill="#1E77FC"
+                        />
+                    </svg>
+                    Add Note
+                </button>
+                <Note
+                    openBox={boxOpen}
+                    currentScore={currentScore}
+                    text={text}
+                    onChangeText={handleChangeText}
+                />
             </div>
         </li>
     );
 }
 
 export default HeuristicItem;
+
+function Note({ openBox, text, onChangeText }) {
+    return (
+        <div className={`mt-3 ${openBox ? "flex" : "hidden"}`}>
+            <textarea
+                className="w-full border border-slate-300"
+                rows="4"
+                value={text}
+                onChange={(ev) => {
+                    onChangeText(ev.target.value);
+                }}
+            ></textarea>
+        </div>
+    );
+}
