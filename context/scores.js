59dbcadd096214ef269aaec83ed404d8ca37329e
Alan Vasconcelos <alandbh@gmail.com>
2022-08-31 21:47:34 -0300

    Adding ID to scores

--- context/scores.js
+++ context/scores.js
@@ -16,6 +16,7 @@ const QUERY_SCORES = gql`
                 journey: { slug: $journeySlug }
             }
         ) {
+            id
             scoreValue
             note
             heuristic {
