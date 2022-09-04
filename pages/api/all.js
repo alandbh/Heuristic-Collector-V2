1e23b49c1f489333b5f61f4753f4172a8d0e360b
Alan Vasconcelos <alandbh@gmail.com>
2022-09-03 22:47:08 -0300

    Formating the scores

--- pages/api/all.js
+++ pages/api/all.js
@@ -2,14 +2,30 @@
 import { gql, useMutation } from "@apollo/client";
 import client from "../../lib/apollo";
 
+async function getData(query, variables) {
+    let queryString = variables
+        ? {
+              query: query,
+              variables,
+          }
+        : {
+              query: query,
+          };
+
+    let result = await client.query(queryString);
+
+    return result;
+}
+
 const QUERY_ALL = gql`
     query getAllPlayers {
         players(where: { project: { slug: "retail-30" } }) {
             id
             name
-            scores {
+            scores(first: 1000) {
                 journey {
                     name
+                    slug
                 }
                 heuristic {
                     heuristicNumber
@@ -21,12 +37,75 @@ const QUERY_ALL = gql`
     }
 `;
 
-export default function handler(req, res) {
-    client
-        .query({
-            query: QUERY_ALL,
-        })
-        .then((data) => {
-            res.status(200).json(data);
+const QUERY_JOURNEYS = gql`
+    query {
+        journeys(where: { project: { slug: "retail-30" } }) {
+            name
+            slug
+        }
+    }
+`;
+
+const QUERY_SCORES = gql`
+    query GetScores(
+        $projectSlug: String
+        $journeySlug: String
+        $playerSlug: String
+    ) {
+        scores(
+            where: {
+                player: { slug: $playerSlug }
+                project: { slug: $projectSlug }
+                journey: { slug: $journeySlug }
+            }
+            first: 1000
+        ) {
+            id
+            scoreValue
+            note
+            evidenceUrl
+            heuristic {
+                heuristicNumber
+            }
+        }
+    }
+`;
+
+export default async function handler(req, res) {
+    const allJourneys = await getData(QUERY_JOURNEYS);
+    const allPlayers = await getData(QUERY_ALL);
+
+    const newPlayerArr = allPlayers.data.players.map(({ id, name, scores }) => {
+        const playerOb = {};
+        playerOb.id = id;
+        playerOb.name = name;
+
+        const scoresByJourney = [];
+
+        const journeys = {};
+
+        allJourneys.data.journeys.map((jou) => {
+            journeys[jou.slug] = scores
+                .filter((score) => {
+                    return score.journey.slug === jou.slug;
+                })
+                .map((score) => {
+                    const heuObj = {};
+                    const scoreObj = {};
+                    scoreObj.scoreValue = score.scoreValue;
+                    scoreObj.note = score.note;
+                    heuObj["h_" + score.heuristic.heuristicNumber] = scoreObj;
+                    return heuObj;
+                });
         });
+
+        playerOb.journeys = journeys;
+
+        return playerOb;
+    });
+    serve(newPlayerArr);
+
+    function serve(data) {
+        res.status(200).json(data);
+    }
 }
