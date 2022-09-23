9f894315e1df3a5248b75a788d34ce62aa818e83
Alan Vasconcelos <alandbh@gmail.com>
2022-09-23 17:14:46 -0300

    Exporting Finding Block

--- components/GroupContainer/index.jsx
+++ components/GroupContainer/index.jsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/router";
 import HeuristicGroup from "../HeuristicGroup";
 import { useScoresContext } from "../../context/scores";
 import { getUnicItem, debounce } from "../../lib/utils";
+import FindingBlock from "../FindingBlock";
 import client from "../../lib/apollo";
 import { BtnSmallPrimary } from "../Button";
 import Switch from "../Switch";
@@ -84,7 +85,7 @@ const MUTATION_PUBLISH_FINDING = gql`
     }
 `;
 
-const MURATION_DELETE_FINDING = gql`
+const MUTATION_DELETE_FINDING = gql`
     mutation DeleteFinding($findingId: ID) {
         deleteFinding(where: { id: $findingId }) {
             id
@@ -380,6 +381,10 @@ function Findings({ data, router, getFindings }) {
                                 finding={finding}
                                 callBack={getFindings}
                                 index={index}
+                                doMutate={doMutate}
+                                client={client}
+                                mutationEdit={MUTATION_FINDINGS}
+                                mutationDelete={MUTATION_DELETE_FINDING}
                             />
                         </li>
                     );
@@ -397,163 +402,6 @@ function Findings({ data, router, getFindings }) {
                 </li>
             </ul>
         </section>
-    );
-}
-
-function FindingBlock({ finding, callBack, index }) {
-    const [text, setText] = useState(finding.findingObject.text || "");
-
-    const [theType, setTheType] = useState(
-        finding.findingObject.theType || "neutral"
-    );
-    const [loading, setLoading] = useState(false);
-    const [status, setStatus] = useState("disabled");
-    const [disabled, setDisabled] = useState(true);
-    const [confirmOpen, setConfirmOpen] = useState(false);
-    const groupRef = useRef(null);
-
-    function onChangeText(value) {
-        setDisabled(false);
-        setStatus("active");
-        setText(value);
-    }
-
-    function onChangeTheType(type) {
-        setTheType(type);
-        setDisabled(false);
-        setStatus("active");
-    }
-
-    function handleClickSaveFinding(id) {
-        setLoading(true);
-        setStatus("loading");
-        setDisabled(true);
-        doMutate(
-            client,
-            {
-                findingId: finding.id,
-                text,
-                theType: theType,
-            },
-            MUTATION_FINDINGS,
-            "edit",
-            null,
-            setStatus
-        );
-    }
-
-    function handleClickDelete() {
-        setConfirmOpen(true);
-    }
-
-    function doDeleteFinding(confirm) {
-        if (confirm) {
-            groupRef.current.style.transition = "0.5s";
-            groupRef.current.style.opacity = "0";
-            groupRef.current.style.transform = "translateX(-80px)";
-
-            setTimeout(() => {
-                setConfirmOpen(false);
-                doMutate(
-                    client,
-                    {
-                        findingId: finding.id,
-                    },
-                    MURATION_DELETE_FINDING,
-                    "delete",
-                    reloadFindingList,
-                    setLoading
-                );
-            }, 500);
-        } else {
-            setConfirmOpen(false);
-        }
-    }
-
-    function reloadFindingList(finding) {
-        // setFindings([...findings, finding]);
-        callBack();
-    }
-
-    return (
-        <div className="flex flex-col gap-3 overflow-x-hidden">
-            <div ref={groupRef} className="px-8">
-                <h3 className="font-bold text-lg mb-4">Finding #{index + 1}</h3>
-                <label
-                    className="text-slate-500 mb-2 inline-block"
-                    htmlFor={"findingText_" + finding.id}
-                >
-                    Type what you`ve found
-                </label>
-                <div className="flex gap-4 flex-col">
-                    <textarea
-                        id={"findingText_" + finding.id}
-                        className="w-full border border-slate-300 dark:border-slate-500 p-2 h-28 text-slate-500 dark:text-slate-300 rounded-md"
-                        rows="3"
-                        value={text}
-                        onChange={(ev) => {
-                            onChangeText(ev.target.value);
-                        }}
-                    ></textarea>
-                    <div
-                        className={`flex ${
-                            confirmOpen ? "justify-center" : "justify-end"
-                        }`}
-                    >
-                        {confirmOpen ? (
-                            <div className="flex gap-2 items-center">
-                                <span className="opacity-60">
-                                    Confirm delete?{" "}
-                                </span>
-                                <button
-                                    className="border px-4 rounded-full h-7 text-red-500 border-red-500 hover:bg-red-700/10"
-                                    onClick={() => doDeleteFinding(true)}
-                                >
-                                    Yes
-                                </button>
-                                <button
-                                    className="px-4 h-7  hover:underline"
-                                    onClick={() => doDeleteFinding(false)}
-                                >
-                                    No
-                                </button>
-                            </div>
-                        ) : (
-                            <button
-                                className="text-red-500 "
-                                onClick={handleClickDelete}
-                            >
-                                Delete
-                            </button>
-                        )}
-                    </div>
-                    <div className="flex justify-between">
-                        <div className="flex flex-col gap-2">
-                            <label className="text-slate-500">
-                                How do you describe this finding?
-                            </label>
-                            <Switch
-                                options={["bad", "neutral", "good"]}
-                                onChange={(theType) => onChangeTheType(theType)}
-                                selected={theType}
-                            />
-                        </div>
-                    </div>
-                    <div className="flex justify-center md:justify-end mb-5 mt-5">
-                        <BtnSmallPrimary
-                            disabled={disabled}
-                            status={status}
-                            textActive={`Save Finding #${index + 1}`}
-                            textFinished="Saved"
-                            onClick={handleClickSaveFinding}
-                        />
-                    </div>
-                </div>
-            </div>
-            <div className="dark:opacity-10">
-                <hr />
-            </div>
-        </div>
     );
 }
 
