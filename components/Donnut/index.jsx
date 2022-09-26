7e814bbedd62da8cfcb34e62dad5b90ce3b55685
Alan Vasconcelos <alandbh@gmail.com>
2022-09-26 16:11:15 -0300

    Improving Donnut

--- components/Donnut/index.jsx
+++ components/Donnut/index.jsx
@@ -1,10 +1,19 @@
 import styles from "./Donnut.module.css";
 
-function Donnut({ total, sum = 0, radius = 34, thick = 6 }) {
+function Donnut({
+    total,
+    sum = 0,
+    radius = 34,
+    thick = 6,
+    color = { base: "#ddd", primary: "#558fe6" },
+}) {
     // const radius = 34;
     const circ = 2 * Math.PI * radius;
     const percent = (sum / total) * 100;
     const size = radius * 2 + thick * 2 + 4;
+    const fontSize = () => {
+        return radius / 3 < 12 ? 12 : radius / 3;
+    };
 
     const dash = {
         // strokeDashoffset: calc(175.92px - (175.92px * 85 / 100));
@@ -18,24 +27,29 @@ function Donnut({ total, sum = 0, radius = 34, thick = 6 }) {
         <div className={styles.percent} style={{ height: size, width: size }}>
             <svg style={{ width: size, height: size }}>
                 <circle
-                    className="stroke-slate-200 dark:stroke-slate-100/30"
+                    // className="stroke-slate-200 dark:stroke-slate-100/30"
                     cx={radius}
                     cy={radius}
                     r={radius}
+                    stroke={color.base}
                     style={{
                         strokeWidth: thick + 2,
                         transform: `translate(${thick + 2}px, ${thick + 2}px)`,
                     }}
                 ></circle>
                 <circle
-                    className="stroke-primary brightness-125"
+                    // className="stroke-primary brightness-125"
                     style={dash}
+                    stroke={color.primary}
                     cx={radius}
                     cy={radius}
                     r={radius}
                 ></circle>
             </svg>
-            <h2 className="dark:text-slate-100/70">
+            <h2
+                style={{ fontSize: fontSize() }}
+                className="dark:text-slate-100/70"
+            >
                 {percent.toFixed(1)}
                 <span>%</span>
             </h2>
