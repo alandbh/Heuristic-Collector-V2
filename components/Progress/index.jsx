ad0fc520adb35dcbeb47f3a8cb41f39303cebad2
Alan Vasconcelos <alandbh@gmail.com>
2022-09-27 02:05:42 -0300

    Customizing Progress bar

--- components/Progress/index.jsx
+++ components/Progress/index.jsx
@@ -1,7 +1,14 @@
 import { useEffect, useState } from "react";
 import styles from "./Progress.module.css";
 
-function Progress({ amount = 10, total = 100, legend, style }) {
+function Progress({
+    amount = 10,
+    total = 100,
+    legend,
+    style,
+    size = "big",
+    barColor = "limeGreen",
+}) {
     const [percentage, setPercentage] = useState(0);
     useEffect(() => {
         const timeout = setTimeout(() => {
@@ -11,16 +18,44 @@ function Progress({ amount = 10, total = 100, legend, style }) {
         return () => clearTimeout(timeout);
     }, [amount, total]);
     return (
-        <div style={{ ...style }} className={styles.container}>
-            {legend && <span className={styles.legend}>{legend}</span>}
+        <div
+            style={{ ...style }}
+            className={
+                size === "small" ? styles.containerSmall : styles.container
+            }
+        >
+            {legend && (
+                <span
+                    className={
+                        size === "small" ? styles.legendSmall : styles.legend
+                    }
+                >
+                    {legend}
+                </span>
+            )}
             <div className={styles["bar-wrapper"]}>
-                <div className={styles.base}>
+                <div
+                    className={
+                        size === "small" ? styles.baseSmall : styles.base
+                    }
+                >
                     <div
-                        style={{ width: percentage + "%" }}
+                        style={{
+                            width: percentage + "%",
+                            background: barColor,
+                        }}
                         className={styles.bar}
                     ></div>
                 </div>
-                <b style={{}}>{percentage}%</b>
+                <b
+                    style={
+                        size === "small"
+                            ? { fontSize: "small", width: "2rem" }
+                            : { fontSize: "1.5rem" }
+                    }
+                >
+                    {percentage}%
+                </b>
             </div>
             <div className={styles.description}>
                 <span className={styles.numbers}>
