42bafacea1b194b708a719081e1f91f9d8b6ad0a
Alan Vasconcelos <alandbh@gmail.com>
2022-12-10 12:20:28 -0300

    Removing supermarket fron the array of scores

--- pages/api/retail-30.js
+++ pages/api/retail-30.js
@@ -1,9 +1,9 @@
 import marketplace from "./results/marketplace.json";
 import fashion from "./results/fashion.json";
 import beauty from "./results/beauty.json";
-import supermarket from "./results/supermarket.json";
+// import supermarket from "./results/supermarket.json";
 
-const results = [...marketplace, ...fashion, ...beauty, ...supermarket];
+const results = [...marketplace, ...fashion, ...beauty];
 
 export default function retail30(req, res) {
     res.status(200).json(results);
