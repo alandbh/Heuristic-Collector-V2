17c831bb1a69069162a197bd291bdac2cee00dd3
Alan Vasconcelos <alandbh@gmail.com>
2022-12-12 17:18:39 -0300

    Fetching data from the separated servers

--- pages/api/retail-30.js
+++ pages/api/retail-30.js
@@ -4,8 +4,27 @@ import beauty from "./results/beauty.json";
 // import supermarket from "./results/supermarket.json";
 // Deploying MKT and Fashion2 in Vercel
 
-const results = [...marketplace, ...fashion, ...beauty];
+export default async function retail30(req, res) {
+    // gettind Marketplace data
+    const marketplaceFetch = await fetch(
+        "https://heuristic-collector-mkt.vercel.app/api/all?project=retail-30"
+    );
+    const marketPlaceJson = await marketplaceFetch.json();
 
-export default function retail30(req, res) {
+    // Getting Fashion data
+    const fashionFetch = await fetch(
+        "https://heuristic-collector-fashion.vercel.app/api/all?project=retail-30"
+    );
+    const fashionJson = await fashionFetch.json();
+
+    // Getting Beauty data
+    const beautyFetch = await fetch(
+        "https://heuristic-collector-v2.vercel.app/api/all?project=retail-30"
+    );
+    const beautyJson = await beautyFetch.json();
+
+    // Concatenating Marketplace, Fashion and Beauty
+    const results = [...marketPlaceJson, ...fashionJson, ...beautyJson];
+
     res.status(200).json(results);
 }
